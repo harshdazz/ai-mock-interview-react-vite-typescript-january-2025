3acb065f458c8115ef9e71941eb8b9e87fb23d1b
Harsh <harshdazz4@gmail.com>
2025-10-11 18:31:32 +0530

    Changes: Header Component Completed

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,6 +1,15 @@
+import { cn } from "@/lib/utils"
+import { useAuth } from "@clerk/clerk-react"
+import { Container } from "lucide-react"
+import { LogoContainer } from "./logo-container"
+import { NavigationRoutes } from "./navigation-routes"
+import { NavLink } from "react-router-dom"
+import ProfileContainer from "./profile-container"
+import ToggleContainer from "../toggle-container"
 
 
 const Header = () => {
+  const {userId} = useAuth()
   return (
     <div>Header</div>
   )
